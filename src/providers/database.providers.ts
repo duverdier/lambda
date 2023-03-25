55363a4ef12de84055c10cfb53ec936c525854cc
Kone Ben Fousseni Christ <foussenichrist@gmail.com>
2023-03-25 23:28:43 +0100

    [LAMBDA] - API - Set up project config

--- src/providers/database.providers.ts
+++ src/providers/database.providers.ts
@@ -12,6 +12,7 @@ export const databaseProviders = [
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        logging: true,
       });
       sequelize.addModels([]);
       await sequelize.sync();
